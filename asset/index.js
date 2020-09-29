a8136217e6d342c26f70ad0c19f511cd58edbad5
tasi788 <tdc@tdccc.com.tw>
2020-09-29 15:37:29 +0800

    ✨ 反正他會動了

--- asset/index.js
+++ asset/index.js
@@ -1,17 +1,18 @@
-var p = new Ping();
+p = new Ping();
 
 var pingTest = [
     "https://doh.futa.gg",
     "https://doh.futa.app",
-    "https://dot.futa.gg"
+    "https://dot.futa.gg",
 ]
+
 // -1 dark
 // < 100 primary
 // > 100 < 200 warning
 // > 200 danger
 function colored(e){
     let result;
-    if (e<=100){
+    if (e>0 & e<=100){
         result = "is-primary";
     } 
     else if (e>100 & e<=200){
@@ -27,23 +28,16 @@ function colored(e){
 }
 
 for (i = 0; i < pingTest.length; i++) {
-    url = new URL(pingTest[i])
-    console.log(url.host)
-    let pingt;
+    let url = new URL(pingTest[i])
     p.ping(url.origin, function (err, data) {
-        console.log(data);
-        var pingt = data;
         if (err) {
             data = -1;
         }
-    });
-
-    template = `<article class="tile is-child notification ${colored(pingt)}" id="${url.host}">
-                <p class="title">${url.origin} -
-                <span id="ping-${url.host}"></span>ms
+        let template = `<article class="tile is-child notification ${colored(data)}" id="${url.host}">
+                <p class="title">${url.host} -
+                <span id="ping-${url.host}">${data}</span>ms
                 </p>
             </article>`
-    document.getElementById("pingbox").innerHTML += template;
-
-    
+        document.getElementById("pingbox").innerHTML += template;
+    });
 }
